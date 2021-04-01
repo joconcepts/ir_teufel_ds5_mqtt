861fcc144f8a73db3f35368d34dcad673ef79544
Jonas Keidel <jonas@jonas-keidel.de>
2021-04-01 20:48:59 +0200

    multithreaded

--- main.py
+++ main.py
@@ -3,92 +3,148 @@
 import paho.mqtt.client as mqtt
 import json
 import time
+import threading
+import uuid
 
 class IRDecoderstation:
-    def __init__(self, client):
-        self.client = client
+    def __init__(self):
+        pass
 
     def power(self):
-        self.send_ir("0x807F50AF", "0x01FE0AF5")
+        return self.ir_cmnd("0x807F50AF", "0x01FE0AF5")
 
     def mute(self):
-        self.send_ir("0x807FD02F", "0x01FE0BF4")
+        return self.ir_cmnd("0x807FD02F", "0x01FE0BF4")
 
     def optical1(self):
-        self.send_ir("0x807F609F", "0x01FE06F9")
+        return self.ir_cmnd("0x807F609F", "0x01FE06F9")
 
     def optical2(self):
-        self.send_ir("0x807FF00F", "0x01FE0FF0")
+        return self.ir_cmnd("0x807FF00F", "0x01FE0FF0")
 
     def mode(self):
-        self.send_ir("0x807FE21D", "0x01FE47B8")
+        return self.ir_cmnd("0x807FE21D", "0x01FE47B8")
 
     def left(self):
-        self.send_ir("0x807FDA25", "0x01FE5BA4")
+        return self.ir_cmnd("0x807FDA25", "0x01FE5BA4")
 
     def right(self):
-        self.send_ir("0x807F1AE5", "0x01FE58A7")
+        return self.ir_cmnd("0x807F1AE5", "0x01FE58A7")
 
     def volume_up(self):
-        self.send_ir("0x807F7A85","0x01FE5EA1")
+        return self.ir_cmnd("0x807F7A85","0x01FE5EA1")
 
     def volume_down(self):
-        self.send_ir("0x807F6A95", "0x01FE56A9")
+        return self.ir_cmnd("0x807F6A95", "0x01FE56A9")
 
     def return_menu(self):
-        self.send_ir("0x807FC03F", "0x01FE03FC")
+        return self.ir_cmnd("0x807FC03F", "0x01FE03FC")
 
-    def send_ir(self, data, data_lsb):
-        data = {"Protocol":"NEC","Bits":32,"Data":data,"DataLSB":data_lsb,"Repeat":0}
-        msg = self.client.publish("cmnd/IR/irsend", json.dumps(data));
-        print(msg.rc)
+    def ir_cmnd(self, data, data_lsb):
+        return {"Protocol":"NEC","Bits":32,"Data":data,"DataLSB":data_lsb,"Repeat":0}
 
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-    client.publish("tele/IRAPI/LWT", "ON")
-    client.subscribe("cmnd/IRAPI/#")
+class Client:
+    def __init__(self, host, port, user, passwd):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.passwd = passwd
+        self.uuid = str(uuid.uuid4())
+        self.ds = IRDecoderstation()
 
-def on_disconnect(client, userdata, flags):
-    client.publish("tele/Fernseher/LWT", "OFF")
+    def connect(self):
+        c = mqtt.Client(client_id=self.uuid)
+        c.username_pw_set(self.user, self.passwd)
+        c.on_connect = self.on_connect
+        c.on_disconnect = self.on_disconnect
+        c.on_message = self.on_message
+        c.on_log = self.on_log
+        c.connect(self.host, self.port, 60)
+        self.client = c
 
-def on_log(client, userdata, level, buf):
-    print("log: ",buf)
+    def on_connect(self, client, userdata, flags, rc):
+        print("Connected with result code "+str(rc))
+        client.publish("tele/IRAPI/LWT", "ON")
+        client.subscribe("cmnd/IRAPI/#")
 
-def power(client, userdata, msg):
-    if msg.payload.decode() == "true":
-        ds.power()
+    def on_disconnect(self, client, userdata, flags):
+        client.publish("tele/IRAPI/LWT", "OFF")
 
-def optical1(client, userdata, msg):
-    if msg.payload.decode() == "true":
-        ds.power()
-        time.sleep(10)
-        ds.optical1()
+    def on_message(self, client, userdata, msg):
+        if msg.payload.decode() != "true":
+            return
 
-def optical2(client, userdata, msg):
-    if msg.payload.decode() == "true":
-        ds.power()
-        time.sleep(10)
-        ds.optical2()
-        time.sleep(0.5)
-        ds.mode()
-        time.sleep(0.5)
-        for i in range(6):
-            ds.right()
-            time.sleep(0.5)
-        ds.return_menu()
+        if msg.topic == "cmnd/IRAPI/Power":
+            arr = self.power()
 
-client = mqtt.Client()
-client.connect("192.168.1.22",1883,60)
-client.username_pw_set("mqtt", "mqtt")
+        if msg.topic == "cmnd/IRAPI/Optical1":
+            arr = self.optical1()
 
-ds = IRDecoderstation(client)
+        if msg.topic == "cmnd/IRAPI/Optical2":
+            arr = self.optical2()
 
-client.on_connect = on_connect
-client.on_disconnect = on_disconnect
-client.on_log = on_log
+        t = threading.Thread(target=self.send_data, args=(arr,))
+        t.start()
 
-client.message_callback_add("cmnd/IRAPI/Power", power)
-client.message_callback_add("cmnd/IRAPI/Optical1", optical1)
-client.message_callback_add("cmnd/IRAPI/Optical2", optical2)
+    def send_data(self, arr):
+        c = Client(HOST, PORT, USER, PASSWD)
+        c.connect()
+        c.client.loop_start()
+    
+        for data in arr:
+            if "sleep" in data:
+                time.sleep(data["sleep"])
+                next
+    
+            c.client.publish("cmnd/IR/irsend", json.dumps(data))
+            time.sleep(0.1)
+    
+        c.client.loop_stop()
+        c.client.disconnect()
 
-client.loop_forever()
+    def on_log(self, client, userdata, level, buf):
+        print("log: ",buf)
+
+    def power(self):
+        arr = [
+                self.ds.power()
+                ]
+        return arr
+
+    def optical1(self):
+        arr = [
+                self.ds.power(),
+                {"sleep": 9},
+                self.ds.optical1()
+                ]
+        return arr
+
+    def optical2(self):
+        arr = [
+                self.ds.power(),
+                {"sleep": 9},
+                self.ds.optical2(),
+                {"sleep": 0.2},
+                self.ds.mode(),
+                {"sleep": 0.2}
+                ]
+        for i in range(5):
+            arr.append(self.ds.left())
+            arr.append({"sleep": 0.2})
+
+        arr.append(self.ds.right())
+        arr.append({"sleep": 0.2})
+
+        arr.append(self.ds.return_menu())
+        return arr
+
+
+HOST = "192.168.1.2"
+PORT = 1883
+USER = "mqtt"
+PASSWD = "mqtt"
+
+main_client = Client(HOST, PORT, USER, PASSWD)
+main_client.connect()
+
+main_client.client.loop_forever()
